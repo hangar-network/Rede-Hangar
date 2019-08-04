79dbf75fb6ad654931f52c40ad815d469c2b051f
Lucas Ricarte <lucasrrt@gmail.com>
2019-08-04 16:53:30 -0300

    local logos

--- src/components/List.vue
+++ src/components/List.vue
@@ -16,7 +16,7 @@
           <td>
             <img
               v-if="startup.logo"
-              :src="`/Rede-Hangar/assets/${startup.logo}`"
+              :src="require(`../assets/${startup.logo}`)"
               alt="logo"
               width="20px"
               height="20px">
