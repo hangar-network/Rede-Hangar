67be2d8f2d1c334c00eb3cc55309985fbc07d8b9
Lucas Ricarte <lucasrrt@gmail.com>
2019-08-04 00:00:44 -0300

    List component

---
+++ src/source/fake-startups.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Empresa A',
+    description: 'Aprendizado por exercícios',
+    market: 'B2B',
+    'last-round': 'Vendida para o X',
+    status: 'Saída',
+    site: 'https://google.com.br',
+  },
+  {
+    name: 'Empresa B',
+    description: 'Sistema de gestão escolar',
+    market: 'B2B',
+    'last-round': 'Seed Capital',
+    status: 'Operação',
+    site: 'https://google.com.br',
+  },
+  {
+    name: 'Empresa C',
+    description: 'Machine Learning para recomendação de conteúdo',
+    market: 'B2C',
+    'last-round': 'Venture Capital',
+    status: 'Escala',
+    site: 'https://google.com.br',
+  },
+];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" width="300px">
+    <List />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import List from '../components/List.vue';
 
 export default Vue.extend({
   name: 'home',
   components: {
+    List,
   },
 });
 </script>
