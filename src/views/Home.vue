bb7f6af7df760baff7591c4d3a7d362ed1dd47db
Lucas Ricarte <lucasrrt@gmail.com>
2019-08-04 17:34:48 -0300

    last modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <Navbar />
+    <div class="last-modified">
+      <p class="last-modified__text">Última modificação em: 04 de Agosto de 2019</p>
+    </div>
     <List />
   </div>
 </template>
@@ -18,3 +21,16 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="less" scoped>
+.last-modified {
+  width: 100%;
+
+  &__text {
+    float: left;
+    float: right;
+    font-size: 13px;
+    margin: 10px 40px 0 0;
+  }
+}
+</style>
