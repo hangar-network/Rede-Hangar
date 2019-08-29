c50a19ce2e10bcdd11f061f45e262ead34b1e18c
Lucas Ricarte <lucasrrt@gmail.com>
2019-08-29 14:56:26 -0300

    deploy

---
+++ precache-manifest.0e5bced92f2487994317ddfa31325ce7.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d36921be67fb8482bfd7324bd86790",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-Light.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Rede-Hangar/robots.txt"
+  },
+  {
+    "revision": "bce66789b1133e57ffac3db20191c056",
+    "url": "/Rede-Hangar/assets/control.png"
+  },
+  {
+    "revision": "32632b299bb23a9aab65",
+    "url": "/Rede-Hangar/js/chunk-vendors.275ae397.js"
+  },
+  {
+    "revision": "a1b276e661d074279e49ac2fcb2fe85b",
+    "url": "/Rede-Hangar/assets/triangle.png"
+  },
+  {
+    "revision": "ff69f9b4ade382505c759d299d84a63a",
+    "url": "/Rede-Hangar/img/vis.ff69f9b4.png"
+  },
+  {
+    "revision": "f276d0e0a6dd3735cdf5",
+    "url": "/Rede-Hangar/js/app.415c2a28.js"
+  },
+  {
+    "revision": "5d0d252033cb8dc3c58f2dcb55af0d13",
+    "url": "/Rede-Hangar/img/voa.5d0d2520.png"
+  },
+  {
+    "revision": "655a250e048e6297074d87871ed6d33c",
+    "url": "/Rede-Hangar/index.html"
+  },
+  {
+    "revision": "f29b441e3bb9ef543dadd11b084ab663",
+    "url": "/Rede-Hangar/img/xr.f29b441e.png"
+  },
+  {
+    "revision": "83c2e01412bb15051a539768529bdafe",
+    "url": "/Rede-Hangar/fonts/Comfortaa/Comfortaa-Medium.ttf"
+  },
+  {
+    "revision": "22e5f38dfe16119bc530442963b45bd1",
+    "url": "/Rede-Hangar/fonts/Comfortaa-Bold.22e5f38d.ttf"
+  },
+  {
+    "revision": "180cd4d9f726ddc62386dedc8fb1a878",
+    "url": "/Rede-Hangar/img/faz.180cd4d9.png"
+  },
+  {
+    "revision": "bce66789b1133e57ffac3db20191c056",
+    "url": "/Rede-Hangar/img/control.bce66789.png"
+  },
+  {
+    "revision": "4b4d056fea0b2039a9575994f2fa41c8",
+    "url": "/Rede-Hangar/img/bh.4b4d056f.png"
+  },
+  {
+    "revision": "de8ec07d3283fb96739b7fb4cb490f5b",
+    "url": "/Rede-Hangar/img/barkus.de8ec07d.png"
+  },
+  {
+    "revision": "74d36921be67fb8482bfd7324bd86790",
+    "url": "/Rede-Hangar/fonts/NunitoSans-Light.74d36921.ttf"
+  },
+  {
+    "revision": "a149931ddfa1797bb05fdd4cb4493fb7",
+    "url": "/Rede-Hangar/img/paperx.a149931d.png"
+  },
+  {
+    "revision": "31bc69eb36f10539e600cbcfb082ec57",
+    "url": "/Rede-Hangar/img/ieg.31bc69eb.png"
+  },
+  {
+    "revision": "3c4a8019ef8aa0f0716f8c9ca8cce70d",
+    "url": "/Rede-Hangar/img/FFAC.3c4a8019.png"
+  },
+  {
+    "revision": "9e4010d3e1abd40c80769d4592338031",
+    "url": "/Rede-Hangar/img/cody.9e4010d3.png"
+  },
+  {
+    "revision": "2d8e1f78f8fab4fb4ac20e4e3dc9a638",
+    "url": "/Rede-Hangar/img/logo.2d8e1f78.png"
+  },
+  {
+    "revision": "a1b276e661d074279e49ac2fcb2fe85b",
+    "url": "/Rede-Hangar/img/triangle.a1b276e6.png"
+  },
+  {
+    "revision": "d3bfe5c6af6d911200bc6792a1380302",
+    "url": "/Rede-Hangar/img/sun.d3bfe5c6.png"
+  },
+  {
+    "revision": "c2be3a9fd7c52fcb71a7566a6d06e2b4",
+    "url": "/Rede-Hangar/img/jg.c2be3a9f.png"
+  },
+  {
+    "revision": "8306712a52d9ec50ae8f62c68cc30fb0",
+    "url": "/Rede-Hangar/img/arvore.8306712a.png"
+  },
+  {
+    "revision": "22e5f38dfe16119bc530442963b45bd1",
+    "url": "/Rede-Hangar/fonts/Comfortaa/Comfortaa-Bold.ttf"
+  },
+  {
+    "revision": "88c901fca95ae46fa1584e61c60aa65d",
+    "url": "/Rede-Hangar/img/liber.88c901fc.png"
+  },
+  {
+    "revision": "3c6ca80ffa0deb6bb65bea6ad4aa0e9a",
+    "url": "/Rede-Hangar/img/elfus.3c6ca80f.png"
+  },
+  {
+    "revision": "830a322efd449853e0cc57e9d09593d6",
+    "url": "/Rede-Hangar/img/lecionas.830a322e.png"
+  },
+  {
+    "revision": "243cbd894539fc02ff3e218ccff0897e",
+    "url": "/Rede-Hangar/icone azul.png"
+  },
+  {
+    "revision": "2cc9166a000920ba17e70675b65f2a1a",
+    "url": "/Rede-Hangar/fonts/Comfortaa/Comfortaa-Light.ttf"
+  },
+  {
+    "revision": "2c80fbc68e3811f3ce13b979973aec72",
+    "url": "/Rede-Hangar/img/contemp.2c80fbc6.png"
+  },
+  {
+    "revision": "8891f7fe67794205a7d34ce42c6e3f70",
+    "url": "/Rede-Hangar/fonts/Comfortaa/Comfortaa-Regular.ttf"
+  },
+  {
+    "revision": "bd318b58018198a57723f311627492ac",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-SemiBold.ttf"
+  },
+  {
+    "revision": "4d52c063e3ac76bd426908bd408550e9",
+    "url": "/Rede-Hangar/fonts/Comfortaa/Comfortaa-SemiBold.ttf"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/Rede-Hangar/fonts/Comfortaa/Comfortaa-VariableFont:wght.ttf"
+  },
+  {
+    "revision": "11bffad5e11322af2e8b54132a46330f",
+    "url": "/Rede-Hangar/fonts/Comfortaa/OFL.txt"
+  },
+  {
+    "revision": "d95152ab5a160491d28b3fce25bf4ff2",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-Black.ttf"
+  },
+  {
+    "revision": "75ec9078a3f7472f3cdee1d312a390a2",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-BlackItalic.ttf"
+  },
+  {
+    "revision": "08e53a516d2ba719d98da46c49b3c369",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-Bold.ttf"
+  },
+  {
+    "revision": "655ce9395fcf8c21f45cfeca5bb280a4",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-BoldItalic.ttf"
+  },
+  {
+    "revision": "2ae455ab84d04fec2d436151e712848f",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "505a059580cfbeaccdcb7a489bb67ec9",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-ExtraBold.ttf"
+  },
+  {
+    "revision": "6aea75496b0ccb484d81a97920d2e64c",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-ExtraLight.ttf"
+  },
+  {
+    "revision": "cf8d9c6c81866d3bdfc1f08d6ea80d8d",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-ExtraLightItalic.ttf"
+  },
+  {
+    "revision": "4c8f447011eef80831b45edb1e5971e0",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-Regular.ttf"
+  },
+  {
+    "revision": "b16342e303cde3bafe2d8746be885ca2",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "d395ff0f45e6b030608de646ec278a35",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-LightItalic.ttf"
+  },
+  {
+    "revision": "2d517b40dabe232416b73e3a721dc950",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/NunitoSans-RegularItalic.ttf"
+  },
+  {
+    "revision": "5fa94515b0e07aa75de1465a1a673ddd",
+    "url": "/Rede-Hangar/fonts/Nunito_Sans/OFL.txt"
+  },
+  {
+    "revision": "f276d0e0a6dd3735cdf5",
+    "url": "/Rede-Hangar/css/app.d7ec2d40.css"
+  },
+  {
+    "revision": "d3bfe5c6af6d911200bc6792a1380302",
+    "url": "/Rede-Hangar/assets/sun.png"
+  },
+  {
+    "revision": "31bc69eb36f10539e600cbcfb082ec57",
+    "url": "/Rede-Hangar/assets/ieg.png"
+  },
+  {
+    "revision": "2d8e1f78f8fab4fb4ac20e4e3dc9a638",
+    "url": "/Rede-Hangar/Logo azul.png"
+  }
+];
